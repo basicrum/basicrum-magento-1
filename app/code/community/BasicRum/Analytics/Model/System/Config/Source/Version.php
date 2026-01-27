6711e0e49f7aa6473918fa7afefe711f1499aaec
Tsvetan Stoychev <ceckoslab@gmail.com>
2026-01-27 22:04:51 +0100

    Now we display the boomerang version and size in the admin area.

---
+++ app/code/community/BasicRum/Analytics/Model/System/Config/Source/Version.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+class BasicRum_Analytics_Model_System_Config_Source_Version
+{
+    /**
+     * Options getter
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return array(
+            array('value' => 'v1.815.60', 'label' => '1.815.60 - cutting-edge - 29 KB (gzipped)')
+        );
+    }
+}
