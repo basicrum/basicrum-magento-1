c55bd698b7e9b795ff49d9560d855f06cb52d783
Tsvetan Stoychev <ceckoslab@gmail.com>
2026-02-25 21:41:18 +0100

    Added more mappings of layout handles and page types .

--- app/code/community/BasicRum/Analytics/Helper/PageTypeDetector.php
+++ app/code/community/BasicRum/Analytics/Helper/PageTypeDetector.php
@@ -31,6 +31,17 @@ class BasicRum_Analytics_Helper_PageTypeDetector extends Mage_Core_Helper_Abstra
             'customer_account_logoutsuccess' => 'Logout Success',
             'contacts_index_index' => 'Contact',
             'sales_guest_form' => 'Orders and Returns',
+            'customer_account_edit' => 'Customer Account Edit',
+            'sales_order_view' => 'Order View',
+            'sales_billing_agreement_index' => 'Billing Agreements',
+            'sales_billing_agreement_view' => 'Billing Agreement View',
+            'sales_guest_view' => 'Guest Order View',
+            'customer_address_form' => 'Customer Address Edit',
+            'customer_address_index' => 'Customer Address List',
+            'wishlist_index_configure' => 'Wishlist Item Configure',
+            'wishlist_index_index' => 'Wishlist Items List',
+            'sales_order_history' => 'Order History',
+            'customer_account_forgotpassword' => 'Forgot Password',
         ];
 
         foreach ($pageTypeMap as $handle => $pageType) {
